ec9bdd333ff675c9070bc3c6f29a76f56f24c4ba
Gabriel Venezian <gabriel.venezian@hotmail.com>
2022-12-04 01:42:35 -0300

    Add QuizQuestionsActivity layout file, populate questions from questionsList to UI

--- app/src/main/java/com/example/flagquiz/MainActivity.kt
+++ app/src/main/java/com/example/flagquiz/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.flagquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btn_start.setOnClickListener {
+            if (player_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please input your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
