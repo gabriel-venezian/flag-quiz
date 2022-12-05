ad1619c225602e765f2cff5164709e16b4714565
Gabriel Venezian <gabriel.venezian@hotmail.com>
2022-12-05 02:19:12 -0300

    Pass username and score details to result screen, update submit button behavior

--- app/src/main/java/com/example/flagquiz/MainActivity.kt
+++ app/src/main/java/com/example/flagquiz/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-
-        player_name.requestFocus()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         btn_start.setOnClickListener {
             if (player_name.text.toString().isEmpty()) {
                 Toast.makeText(this, "Please input your name", Toast.LENGTH_SHORT).show()
             } else {
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, player_name.text.toString())
                 startActivity(intent)
                 finish()
             }
--- app/src/main/java/com/example/flagquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/flagquiz/QuizQuestionsActivity.kt
@@ -1,13 +1,12 @@
 package com.example.flagquiz
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.View.OnClickListener
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
@@ -15,10 +14,14 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
+    private var mCorrectAnswers: Int = 0
+    private var mUserName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
 
         mQuestionsList = Constants.getQuestions()
 
@@ -33,6 +36,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun setQuestion() {
+        btn_submit.isEnabled = false
         val question = mQuestionsList!![mCurrentPosition - 1]
 
         defaultOptionsView()
@@ -50,24 +54,32 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         iv_image.setImageResource(question.image)
 
         tv_option_one.text = question.optionOne
+        tv_option_one.isClickable = true
         tv_option_two.text = question.optionTwo
+        tv_option_two.isClickable = true
         tv_option_three.text = question.optionThree
+        tv_option_three.isClickable = true
         tv_option_four.text = question.optionFour
+        tv_option_four.isClickable = true
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.tv_option_one -> {
                 selectedOptionView(tv_option_one, selectedOptionNumber = 1)
+                btn_submit.isEnabled = true
             }
             R.id.tv_option_two -> {
                 selectedOptionView(tv_option_two, selectedOptionNumber = 2)
+                btn_submit.isEnabled = true
             }
             R.id.tv_option_three -> {
                 selectedOptionView(tv_option_three, selectedOptionNumber = 3)
+                btn_submit.isEnabled = true
             }
             R.id.tv_option_four -> {
                 selectedOptionView(tv_option_four, selectedOptionNumber = 4)
+                btn_submit.isEnabled = true
             }
             R.id.btn_submit -> {
                 if (mSelectedOptionPosition == 0) {
@@ -78,18 +90,26 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                             setQuestion()
                         }
                         else -> {
-                            Toast.makeText(
-                                this,
-                                "You have successfully completed the Quiz!",
-                                Toast.LENGTH_SHORT
-                            ).show()
+                            val intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                            intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionsList!!.size)
+                            startActivity(intent)
+                            finish()
                         }
                     }
                 } else {
+                    tv_option_one.isClickable = false
+                    tv_option_two.isClickable = false
+                    tv_option_three.isClickable = false
+                    tv_option_four.isClickable = false
+
                     val question = mQuestionsList?.get(mCurrentPosition - 1)
 
                     if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswers++
                     }
 
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
